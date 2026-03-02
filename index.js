b86541629a6eeaecaeeebed5483b6a5059173729
baoquangc <119931532+baoquangc@users.noreply.github.com>
2026-03-02 12:38:43 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Menu</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    body {
+      margin: 0;
+      background: black;
+    }
+    img {
+      width: 100%;
+      height: auto;
+      display: block;
+    }
+  </style>
+</head>
+<body>
+  <img src="menu.jpg">
+</body>
+</html>
